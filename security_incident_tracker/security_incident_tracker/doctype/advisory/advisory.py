cf8d46f604d3f247239418711140c1ed4b1521b3
NjengaSheba <njengasheba@gmail.com>
2025-09-09 19:51:54 +0300

    initial commit

---
+++ security_incident_tracker/security_incident_tracker/doctype/advisory/advisory.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Kelvin Njenga and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class Advisory(Document):
+	pass
